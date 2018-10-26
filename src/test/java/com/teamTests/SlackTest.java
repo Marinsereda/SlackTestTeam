bf7fb6631b70e4d05881dcfb26db3eb546926b28
Marina <seredamv92@gmail.com>
2018-10-26 21:18:27 +0300

    2 test + gitignore chromedrive

--- src/test/java/com/teamTests/SlackTest.java
+++ src/test/java/com/teamTests/SlackTest.java
@@ -1,8 +1,32 @@
 package com.teamTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class SlackTest {
+public class SlackTest extends TestBase{
     private static WebDriver browser;
+    static Helper h;
+
+
+    @Test
+    public static void testSignOut(){
+//        login;
+        browser.findElement(By.cssSelector("#team_menu")).click();
+        browser.findElement(By.cssSelector("#menu_items_scroller li#logout.logout_url")).click();
+
+        Assert.assertTrue(browser.findElements(By.cssSelector("#team_menu_user")).size()>0);
+
+    }
+    @Test
+    public static void testSlackbot(){
+//        login;
+        browser.findElement(By.cssSelector(".p-channel_sidebar__channel--im-slackbot")).click();
+        h.findAndFill(By.cssSelector(".msg_input"), TestData.message + "\n");
+
+        Assert.assertTrue(browser.findElements(By.cssSelector("#team_menu_user")).size()>0);
+
+    }
 
 }
--- src/test/java/com/teamTests/TestData.java
+++ src/test/java/com/teamTests/TestData.java
@@ -5,6 +5,7 @@ public class TestData {
     static String workSpace = "hillelQA";
     static String login = "apple.incor@gmail.com";
     static String password = "Password2018";
-    static String siteLink = "https://slack.com/signin";
+    static String siteLink = "https://slack.com/";
+    static String message = "Hello Slack";
 
 }
