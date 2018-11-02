2f532d2b525c64d5700ee21dd49057d6a13d6ae5
KPukhalenko <kpukhalenko@gmail.com>
2018-11-02 21:19:12 +0200

    reactored getMessage function

--- src/test/java/com/teamTests/SlackTest.java
+++ src/test/java/com/teamTests/SlackTest.java
@@ -16,20 +16,6 @@ public class SlackTest extends TestSteps {
         Assert.assertTrue(browser1.getCurrentUrl().contains(TestData.protocol + TestData.workSpace + "." + TestData.siteLink));
     }
 
-//    @Test
-//    static void connectToWorkSpaceSuccess2()throws Exception{
-//        connectToWorkSpace2(TestData.workSpace);
-//        Assert.assertTrue(browser2.getCurrentUrl().contains(TestData.protocol + TestData.workSpace + "." + TestData.siteLink));
-//    }
-
-
-//    static void login2 (String username, String password) throws Exception{
-//        browser2.get(TestData.protocol + TestData.workSpace + "." + TestData.siteLink);
-//        h2.findAndFill(By.cssSelector("#email"), username);
-//        h2.findAndFill(By.cssSelector("#password"), password + "\n");
-
-//    }
-
     @Test (dependsOnMethods = "connectToWorkSpaceSuccess", alwaysRun = true , priority = -1)
     static void loginFail() throws Exception{
         login("bad@login.com","badPass");
@@ -41,12 +27,6 @@ public class SlackTest extends TestSteps {
         login(TestData.login_1,TestData.password_1);
         Assert.assertTrue(browser1.findElements(By.cssSelector("#team_menu_user_name")).size()>0);
     }
-//    @Test (dependsOnMethods = "connectToWorkSpaceSuccess2", alwaysRun = true)
-//    static void loginSuccess2() throws Exception{
-//        login2(TestData.login_2,TestData.password_2);
-//        Assert.assertTrue(browser2.findElements(By.cssSelector("#team_menu_user_name")).size()>0);
-
-//    }
 
     @Test (dependsOnMethods = "loginSuccess")
     static void sendMessageToUser() {
@@ -61,26 +41,10 @@ public class SlackTest extends TestSteps {
     }
 
     @Test(dependsOnMethods = "sendMessageToUser")
-    static void getMessage() {
-//        browser2.get(TestData.protocol + TestData.workSpace + "." + TestData.siteLink + "messages/");
-
-        new FluentWait<>(browser2)
-                .withTimeout(Duration.ofSeconds(7))
-                .pollingEvery(Duration.ofSeconds(1)).ignoring(Exception.class)
-                .until(browser2 -> browser2.findElement(By.xpath("//span[text()='" + TestData.userName_1 + "']")))
-                .click();
-
-        /*this option may also be used*/
-//        browser2.findElement(By.xpath("//span[text()='"+ TestData.userName_1 +"']")).click();
-        Assert.assertTrue(browser2.findElement(By.cssSelector("button[id='im_title']")).getText().contains(TestData.userName_1));
-
-        String selector = "//span[@class='c-message__body' and text() = '" + TestData.messageText + "']";
-        Assert.assertEquals(browser2.findElements(By.xpath(selector)).size(),1);
-
-        /*output message in console*/
-        System.out.println(TestData.userName_2 + " has received message from " + TestData.userName_1 + ": " + browser2.findElement(By.xpath(selector)).getText());
+    static void getMessageFromUser() {
+        String messageFromUser = getMessage(TestData.userName_1, TestData.messageText);
+        Assert.assertEquals(messageFromUser, TestData.messageText);
     }
-
 
 
     @Test (dependsOnMethods = "loginSuccess" , priority = 20)
@@ -91,3 +55,26 @@ public class SlackTest extends TestSteps {
 
 }
 
+
+
+/* stuff for user #2*/
+//    @Test
+//    static void connectToWorkSpaceSuccess2()throws Exception{
+//        connectToWorkSpace2(TestData.workSpace);
+//        Assert.assertTrue(browser2.getCurrentUrl().contains(TestData.protocol + TestData.workSpace + "." + TestData.siteLink));
+//    }
+
+
+//    static void login2 (String username, String password) throws Exception{
+//        browser2.get(TestData.protocol + TestData.workSpace + "." + TestData.siteLink);
+//        h2.findAndFill(By.cssSelector("#email"), username);
+//        h2.findAndFill(By.cssSelector("#password"), password + "\n");
+
+//    }
+
+//    @Test (dependsOnMethods = "connectToWorkSpaceSuccess2", alwaysRun = true)
+//    static void loginSuccess2() throws Exception{
+//        login2(TestData.login_2,TestData.password_2);
+//        Assert.assertTrue(browser2.findElements(By.cssSelector("#team_menu_user_name")).size()>0);
+
+//    }
--- src/test/java/com/teamTests/TestSteps.java
+++ src/test/java/com/teamTests/TestSteps.java
@@ -1,8 +1,12 @@
 package com.teamTests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class TestSteps extends TestBase {
 
@@ -18,16 +22,32 @@ public class TestSteps extends TestBase {
         h1.findAndFill(By.cssSelector("#password"), password + "\n");
     }
 
-    static void sendMessage(String name, String text) {
-        browser1.findElement(By.xpath("//span[text()='"+ name +"']")).click();
-        Assert.assertTrue(browser1.findElement(By.cssSelector("button[id='im_title']")).getText().contains(name));
+    static void sendMessage(String toUser, String text) {
+        browser1.findElement(By.xpath("//span[text()='"+ toUser +"']")).click();
+        Assert.assertTrue(browser1.findElement(By.cssSelector("button[id='im_title']")).getText().contains(toUser));
 
         Actions actions = new Actions(browser1);
         actions.moveToElement(browser1.findElement(By.id("msg_input"))).click();
         actions.sendKeys(text + "\n").build().perform();
     }
 
-    static void signOut() {
+    static String getMessage(String fromUser, String message) {
+
+        new FluentWait<>(browser2)
+                .withTimeout(Duration.ofSeconds(7))
+                .pollingEvery(Duration.ofSeconds(1)).ignoring(Exception.class)
+                .until(browser2 -> browser2.findElement(By.xpath("//span[text()='" + fromUser + "']")))
+                .click();
+
+        /*this option may also be used*/
+//        browser2.findElement(By.xpath("//span[text()='"+ TestData.userName_1 +"']")).click();
+        Assert.assertTrue(browser2.findElement(By.cssSelector("button[id='im_title']")).getText().contains(fromUser));
+
+        String selector = "//span[@class='c-message__body' and text() = '" + message + "']";
+        return browser2.findElement(By.xpath(selector)).getText();
+    }
+
+        static void signOut() {
         browser1.findElement(By.cssSelector("#team_menu")).click();
         browser1.findElement(By.cssSelector("#menu_items_scroller li#logout.logout_url")).click();
     }
